2631e935ca72029c817ee170b2432a9e9e123bd2
alce65 <alce65@hotmail.es>
2019-07-05 16:22:00 +0200

    Inicio factura

---
+++ JS_OOP/15_facturas.js
@@ -0,0 +1,29 @@
+/* 
+Definir la estructura de un objeto que almacena una factura. 
+
+Las facturas están formadas por:
+- la información de la propia empresa:
+    - nombre de la empresa
+    - dirección
+    - teléfono
+    - NIF 
+- la información del cliente
+    - nombre de la empresa
+    - dirección
+    - teléfono
+    - NIF 
+- una lista de elementos, cada uno de los cuales dispone de:
+    - descripción
+    - precio por unidad
+    - cantidad 
+- otra información básica de la factura
+    - el número de dactura
+    - tipo de IVA
+    - forma de pago
+    - la fecha
+
+Añadir al menos dos métodos:
+- el que calcula el importe total de la factura
+- el que muestra por pantalla la factura 
+    o al menos el importe total de la factura.
+*/
