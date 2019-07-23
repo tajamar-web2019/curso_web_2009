77cd9699edbb63d10c8676b6e47d3a00defff4f0
alce65 <alce65@hotmail.es>
2019-07-23 21:23:23 +0200

    Añadir dinámicamente al array

--- Web_JS/06_salir/js/app.js
+++ Web_JS/06_salir/js/app.js
@@ -8,17 +8,28 @@ export function app() {
     let btnCancelar = document.querySelector('#btn-cancelar')
     let dlgTime = document.querySelector("#dlg-time")
     let wrapperCronos = document.querySelector(".wrapper-cronos")
+    let inPlayer = document.querySelector('#in-player')
+    let btnInscribir = document.querySelector('#btn-inscribir')
     let handlerSalir
     let handlerInterval
 
     btnSalir.addEventListener('click', onClick)
     btnCancelar.addEventListener('click', onCancel)
+    btnInscribir.addEventListener('click', onInscribir)
 
-    let aCronos = [1, 2, 3, 4]
-    aCronos.forEach( idCrono => { 
-        new Crono(idCrono, wrapperCronos)
-    })
-    // wrapperCronos.innerHTML += `<p>Creados los cronómetros</p>`
+    let aCronos =  []
+    if (localStorage.getItem('players')) {
+        aCronos = JSON.parse(localStorage.getItem('players'))
+    }
+    checkArray()
+
+    function checkArray() {
+        wrapperCronos.innerHTML = ''
+        aCronos.forEach( idCrono => { 
+            new Crono(idCrono, wrapperCronos)
+        })
+        // wrapperCronos.innerHTML += `<p>Creados los cronómetros</p>`    
+    }
 
     function onClick() {
         let url = 'https://www.google.com/'
@@ -35,5 +46,17 @@ export function app() {
         clearTimeout(handlerSalir)
         clearInterval(handlerInterval)
         dlgCancelar.close()
+    }
+
+    function onInscribir() {
+        let player = inPlayer.value
+        if (player) {
+            aCronos.push(player)
+            localStorage.setItem('players', JSON.stringify(aCronos))
+            inPlayer.value = ''
+            console.log(aCronos)
+        }
+
+        checkArray()
     }
  }
