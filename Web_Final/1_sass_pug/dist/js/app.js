89112c86dc367cd71b38da45261001a57dd586cd
alce65 <alce65@hotmail.es>
2019-09-18 17:59:32 +0200

    Scaffolding con dist

---
+++ Web_Final/1_sass_pug/dist/js/app.js
@@ -0,0 +1,1 @@
+function main(){console.log("Cargado app.js")}document.addEventListener("DOMContentLoaded",main);
---
+++ Web_Final/1_sass_pug/js/app.js
@@ -0,0 +1,7 @@
+
+function main() {
+    var mensaje = 'Cargado app.js'
+    console.log(mensaje)
+}
+
+document.addEventListener('DOMContentLoaded', main)
